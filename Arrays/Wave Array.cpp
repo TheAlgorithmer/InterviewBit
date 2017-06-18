dc8dc65feb8d9febd6d4ee8427773b4519c6eed1
rrzschool <rrzschool@gmail.com>
2017-06-18 16:04:12 +0300

    Create Wave Array.cpp

---
+++ Arrays/Wave Array.cpp
@@ -0,0 +1,10 @@
+vector<int> Solution::wave(vector<int> &arr) {
+
+    int len=arr.size();
+
+    sort(arr.begin(),arr.end());
+    
+    for(int i=0; i<len-1; i+=2) swap(arr[i],arr[i+1]);
+    
+    return arr;
+}
